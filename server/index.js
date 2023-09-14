99ddf0f18c08414f032ac9ecaf99153234a26970
ayushkumar05 <ayushk0520@gmail.com>
2023-09-15 01:02:45 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -6,10 +6,9 @@ import cookieParser from "cookie-parser";
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auths.js";
 import tweetRoutes from "./routes/tweets.js";
-
 const app = express();
 dotenv.config();
-
+var cors = require('cors')
 const connect = () => {
   mongoose.set("strictQuery", false);
   mongoose
@@ -21,7 +20,7 @@ const connect = () => {
       throw err;
     });
 };
-
+app.use(cors())
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/users", userRoutes);
