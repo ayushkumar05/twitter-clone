c815a23c8a81be1533f78067c9368ff09d0a8f7c
ayushkumar05 <ayushk0520@gmail.com>
2023-09-15 21:25:32 +0530

    bug fixes

--- server/index.js
+++ server/index.js
@@ -21,9 +21,7 @@ const connect = () => {
       throw err;
     });
 };
-app.use(cors({
-  origin:['http://localhost:3000','https://ayushkumar05.github.io/']
-}));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/users", userRoutes);
