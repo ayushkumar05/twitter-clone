69396c81140271b92d97712ffef8c7927efec3a2
ayushkumar05 <ayushk0520@gmail.com>
2023-09-15 22:13:22 +0530

    origin allow

--- server/index.js
+++ server/index.js
@@ -21,11 +21,7 @@ const connect = () => {
       throw err;
     });
 };
-app.use(cors(
-    {
-      origin:'https://ayushkumar05.github.io/'
-    }
-));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/users", userRoutes);
