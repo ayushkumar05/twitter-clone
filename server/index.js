bc736f24a987b6239acd3a9eb75738593e029bf6
ayushkumar05 <ayushk0520@gmail.com>
2023-09-15 22:54:51 +0530

    credddd1

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const connect = () => {
       throw err;
     });
 };
-app.use(cors({credentials: true, origin: 'https://ayushkumar05.github.io/'}));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/users", userRoutes);
