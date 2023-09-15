b0e100ba9fa9fff06ad9c642284163861e15a8dc
ayushkumar05 <ayushk0520@gmail.com>
2023-09-15 16:36:06 +0530

    env files

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auths.js";
 import tweetRoutes from "./routes/tweets.js";
+
 const app = express();
 dotenv.config();
 const cors= require('cors')
@@ -20,7 +21,7 @@ const connect = () => {
       throw err;
     });
 };
-app.use(cors())
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/users", userRoutes);
